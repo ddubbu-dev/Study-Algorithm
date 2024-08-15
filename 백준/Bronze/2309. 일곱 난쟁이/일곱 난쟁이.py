31043d572460821d3efd7b3866265b8300aaa366
SEON MI KIM <dev.ddubbu@gmail.com>
2024-08-15 09:23:28 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,39 +1,17 @@
-"""
-[문제 이해]
-- 9명 중 7명 키 합을 구해서 = 100인 경우 
-- 난쟁이들 키 오름차순 출력
-
-[풀이]
-- 조합으로 9C7 경우의 수 찾기
-"""
+from itertools import combinations
 
 N = 9
-arr = []
-for i in range(N):
-    arr.append(int(input()))
-
-# 탐색 -
-def make조합_중복없음(arr: list, cnt: int):
-    result = []
-
-    def inner(arr, start_idx, picked_arr):
-        if len(picked_arr) == cnt:
-            result.append(picked_arr)
-            return
-        for i in range(start_idx, len(arr)):
-            inner(arr, i + 1, picked_arr[:] + [arr[i]])
-
-    inner(arr, 0, [])
-
-    return result
+nums = [int(input()) for _ in range(N)]
 
 
+cases = combinations(nums, 7)
 
-collections = make조합_중복없음(arr, 7)
 
-for case in collections:
-    acc = sum(case)
+for case in cases:
+    sorted_case = sorted(list(case))
+    acc = sum(sorted_case)
 
     if acc == 100:
-        print("\n".join(map(str, sorted(case))))
+        for item in sorted_case:
+            print(item)
         break
