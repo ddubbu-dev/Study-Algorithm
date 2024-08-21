8140363edf9b16d8a00e2660d64c8c2c563cf3dd
SEON MI KIM <dev.ddubbu@gmail.com>
2024-08-21 17:01:08 +0900

    [Silver II] Title: DFS와 BFS, Time: 60 ms, Memory: 35156 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,74 +1,53 @@
-from collections import deque
 
 import sys
-readline = lambda: sys.stdin.readline().rstrip()
-# readline = input
+readline = lambda: sys.stdin.readline().strip()
 
-N, M, start_v = map(int, readline().split())
+N, M, V = map(int, readline().split())
+GRAPH_SIZE = N + 1
+graph = [[] for _ in range(GRAPH_SIZE)]
+for _ in range(M):
+    u, v = map(int, readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-G = {}
-for _ in range(M):  # 양방향 간선!
-    v1, v2 = map(int, readline().split())
-
-    if G.get(v1):
-        prev = G[v1]
-        # 작은 것부터 먼저 방문하기 위해 마지막에 넣어줘야함
-        new_child = sorted(prev + [v2], reverse=True)
-        G[v1] = new_child
-    else:
-        G[v1] = [v2]
-
-    if G.get(v2):
-        prev = G[v2]
-        # 작은 것부터 먼저 방문하기 위해 마지막에 넣어줘야함
-        new_child = sorted(prev + [v1], reverse=True)
-        G[v2] = new_child
-    else:
-        G[v2] = [v1]
-
-def dfs():
-    stack = deque([start_v])
-    visited = [False] * (N + 1)
-
-    while stack:
-        v = stack.pop()
-
-        if visited[v]:
-            continue
-
-        print(v, end=" ")
-        visited[v] = True
-
-        try:
-            for child_v in G[v]:  # Q. dict key 값 없을 때 에러 어떻게 나는지 궁금함
-                if not visited[child_v]:
-                    stack.append(child_v)
-        except:
-            continue
+for idx in range(len(graph)): # 작은 정점부터 순회
+    graph[idx] = sorted(graph[idx])
 
 
-def bfs():
-    que = deque([start_v])
-    visited = [False] * (N + 1)
+# ================ DFS ================
+visited = [False] * GRAPH_SIZE
+
+
+def dfs(v):
+    visited[v] = True
+    print(v, end=" ")
+
+    for near in graph[v]:
+        if not visited[near]:
+            dfs(near)
+
+
+dfs(V)
+print()
+
+# ================ BFS ================
+from collections import deque
+
+
+def bfs(v):
+    visited = [False] * GRAPH_SIZE
+    que = deque([v])
 
     while que:
         v = que.popleft()
 
-        if visited[v]:
-            continue
-
-        print(v, end=" ")
         visited[v] = True
+        print(v, end=" ")
 
-        try:
-            child = sorted(G[v])  # dfs와 달리 오름차순으로, 선입선출
-            for child_v in child:  # Q. dict key 값 없을 때 에러 어떻게 나는지 궁금함
-                if not visited[child_v]:
-                    que.append(child_v)
-        except:
-            continue
+        for near in graph[v]:
+            if not visited[near]:
+                que.append(near)
+                visited[near] = True
 
 
-dfs()
-print()
-bfs()
+bfs(V)
