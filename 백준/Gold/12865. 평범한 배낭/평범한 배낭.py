3a1f62c298847dd75f4570db33b67f1da0c5ff4e
SEON MI KIM <dev.ddubbu@gmail.com>
2024-08-26 18:29:25 +0900

    [Gold V] Title: 평범한 배낭, Time: 4776 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,36 +1,31 @@
 """
-[참고자료](https://claude-u.tistory.com/208)
+[공간 최적화]
+1D로 배낭문제 풀기
 """
 
 import sys
 
 readline = lambda: sys.stdin.readline().strip()
-readline = input
+# readline = input
 
-# 1. 전처리
-
-N, K = map(int, readline().split())
-bag = [(0, 0)]  # empty item
-
-
+# 전처리
+N, TARGET_WEIGHT = map(int, readline().split())
+bag = []
 for _ in range(N):
     w, v = map(int, readline().split())
     bag.append((w, v))
 
+# dp[w] = w 무게에서 최대 가치
+dp = [0] * (TARGET_WEIGHT + 1)
+
 WEIGHT = 0
 VALUE = 1
 
-# dp[i][w] = i번째 물건까지 사용해서 w무게를 갖는 최대 가치
-dp = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
-
-for i in range(1, N + 1):
-    for w in range(1, K + 1):
+for i in range(N):
+    for w in range(TARGET_WEIGHT, -1, -1):
         cur_weight = bag[i][WEIGHT]
         cur_value = bag[i][VALUE]
+        if w - cur_weight >= 0:  # 유효한 인덱스 체크
+            dp[w] = max(dp[w], dp[w - cur_weight] + cur_value)
 
-        if w >= cur_weight:  # 과거값으로부터 갱신 가능
-            dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - cur_weight] + cur_value)
-        else:  # 과거값 그대로 갱신
-            dp[i][w] = dp[i - 1][w]
-
-print(dp[N][K])
+print(dp[TARGET_WEIGHT])
